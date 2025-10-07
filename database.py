f517c026f819d8ef8ba4ba8de653afb71775d60a
16vaishu <132426021+16vaishu@users.noreply.github.com>
2025-10-07 21:17:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,33 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# PostgreSQL connection
-DATABASE_URL = "sqlite:///./test.db"
+# Your Cloud SQL instance connection name
+INSTANCE_CONNECTION_NAME = "fastapi-cloudrun-474109:asia-south1:fastapi-sql-instance"
 
+DB_USER = "dbuser"
+DB_PASS = "vaishali123"
+DB_NAME = "mydb"
 
-engine = create_engine(DATABASE_URL)
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            INSTANCE_CONNECTION_NAME,
+            "pg8000",
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
+        )
+        return conn
+
+engine = sqlalchemy.create_engine(
+    "postgresql+pg8000://",
+    creator=getconn,
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency for FastAPI
 def get_db():
     db = SessionLocal()
     try:
